d5db09e1cd7959eea693934411fb20f5c79bbe1b
xi0ni <george.robokos@gmail.com>
2024-10-30 20:08:08 -0400

    latitutde longitude

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("latitude: ");
+      Double int1 = input.nextDouble();
+      System.out.println("longitude: ");
+      Double int2 = input.nextDouble();
+      
+      if(int1 < -90 || int1 > 90){
+        System.out.println("latitude is incorrect");
+      if(int2 < -180 || int2 > 180){
+          System.out.println("longitude is incorrect");
+        }
+        
+  
+      }
+      else
+      {
+        System.out.println("The location:" + int1 + " , " + int2);
+      }
+      
     }
-}
+  }
+  
+  /*if((int1 >= 90 && int1<= 90) && (int2 >= -180 && int2 <= 180))*/
+
+
