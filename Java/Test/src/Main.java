e21cbddb73d312c89cb0b4fe40ccd8b23f2337b6
xi0ni <george.robokos@gmail.com>
2024-11-04 21:18:47 -0500

    origional

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,19 +1,6 @@
 
-public class Main {
-       public static void main(String[] args)
-     {
-         int x = 13;
-         int y = 13;
-         if (x > y && (2 * x / 3 + 3 * y / 4 == x / 3 * 2 + y / 4 * 3)){
-            System.out.print("yes");
-         }
+public class Main{
+   public static void main(String[] args) {
 
-         else{
-            System.out.print("no");
-         }
-       }
-      }
-
-    
-
-
+   }
+}
