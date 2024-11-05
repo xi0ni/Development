39061be3c741362ae097d05b582ee33361691a24
xi0ni <george.robokos@gmail.com>
2024-11-04 21:19:00 -0500

    og

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
-public class Main{
-   public static void main(String[] args) {
+public class Main {
+       public static void main(String[] args)
+       {
+          Scanner input = new Scanner(System.in);
+          
+          System.out.println("latitude: ");
+          double latitude = input.nextDouble();
+          System.out.println("longitude: ");
+          double longitude = input.nextDouble();
+          
+          if (latitude < -90 || latitude > 90) {
+             System.out.println("latitude is incorrect");
+          }
+          if (longitude < -180 || longitude > 180) {
+             System.out.println("longitude is incorrect");
+          }
+          
+          if (latitude >= -90 && latitude <= 90 && longitude >= -180 && longitude <= 180) {
+             System.out.println("The location: " + latitude + " , " + longitude);
+          }
+       }
+    }
 
-   }
-}
+    
+
