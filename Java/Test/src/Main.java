5ca8ff460280ff0be0341a3a8026ee8ff208cd5e
xi0ni <george.robokos@gmail.com>
2024-11-10 13:16:27 -0500

    U3 L5 Activity 1 fixed 100%

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-     public static void main(String[] args)
-  {
-
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter 2 integers:");
-    int x = scan.nextInt();
-    int y = scan.nextInt();
-    if(x/y < 8 && x != 0)
-      System.out.println("Ratio OK");
-
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 2 integers:");
+        
+        int x = input.nextInt();
+        int y = input.nextInt();
+        
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed.");
+        } else {
+            double ratio = (double) x / y; 
+            if (ratio > 1 && ratio <= 8) {
+                System.out.println("Ratio OK");
+            }
+        }
+        
+        input.close();
+    }
     }
 
