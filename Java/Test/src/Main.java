84ce0f05cbc6a26e97a80cfce60b7330702799d9
xi0ni <george.robokos@gmail.com>
2024-11-25 18:07:08 -0500

    odd numbers

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Test x = new Test(123456789); 
-        x.initialize();         
-    }
+            int n = 10; 
+            int sum = 0;
+            for (int i = 1; i <= n; i += 2) {
+                sum += i; 
+            }
+            System.out.println(sum);
+        }
+        
 }
--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -8,11 +8,12 @@ public class Test {
     }
 
     public void initialize() {
-        while (n > 0) {
-            sum += n % 10;
-            n /= 10;     
+        for (int i = 1; i < 100; i++) { 
+            if (n % i != 0) {
+                sum++;
+                System.out.println(i); 
+            }
         }
-
         System.out.println(sum);
     }
 }
