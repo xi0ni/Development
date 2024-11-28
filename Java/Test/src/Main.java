661e71fb465baf3420755adce47874fb9cb69443
xi0ni <george.robokos@gmail.com>
2024-11-27 19:02:44 -0500

    test cheats lmao

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 20; // Starting number (inclusive)
-        while (num >= 0) {
-            if (num % 2 == 0) {
-                System.out.println(num);
-            }
-            num--;
-        }
 
-        for (int i = 20; i >= 0; i--) { 
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
+        int count = 1;
+        while(count <= 8){
+            
+            System.out.println(count+ " ");
+            count = count+1;
         }
-        
+}
 }
