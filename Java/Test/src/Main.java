c96db9a3ae7b43fcb7984228c1a2146f1d64d2e1
xi0ni <george.robokos@gmail.com>
2024-12-05 20:10:42 -0500

    substring

--- Java/Test/src/Main.java
+++ Java/Test/src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        for(int i = 1; i <= 10; i++){
-            for(int j = 1; j <= 10; j++){
-                System.out.print(j*i + " ");
-            }
-            System.out.println("");
-        }
+        String i = "phone";
+        System.out.println(i.substring(2,4));
 }
 }
