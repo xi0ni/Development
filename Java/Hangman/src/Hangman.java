6c2f4d64b5756b1dbbdf8079b6c28349e1153045
xi0ni <george.robokos@gmail.com>
2025-01-13 19:52:13 -0500

    finished

--- Java/Hangman/src/Hangman.java
+++ Java/Hangman/src/Hangman.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
@@ -7,22 +8,26 @@ public class Hangman {
     int guesses;
     boolean x;
     String placeholder;
+    
 
     public Hangman() {
         this.x = true;
-        this.word = "george";
         this.guesses = 5;
         this.placeholder = " ";
 
-        for (int i = 0; i < word.length(); i++) {
-            this.placeholder += "_ ";
+        String[] words = {"george", "apple", "banana"};
+        Random random = new Random();
+        this.word = words[random.nextInt(words.length)];
+
+        for (int i = 0; i < (word.length()-1); i++) {
+            this.placeholder += "-";
         }
 
         System.out.println("placeholder: " + this.placeholder);
         while (x == true) {
             System.out.print("pick a letter: ");
             String letter = input.nextLine();
-            System.out.println("");
+            System.out.println();
             
             
             if (word.contains(letter)) {
@@ -30,6 +35,20 @@ public class Hangman {
                     if (word.substring(i, i + 1).equals(letter)) {
                         this.placeholder = this.placeholder.substring(0, i) + letter + this.placeholder.substring(i + 1);
                     }
+
+                    if(this.placeholder.equals(this.word)){
+                        System.out.println("you win");
+                        x = false;
+                    }
+                }
+            }
+            else {
+                this.guesses -= 1;
+                System.out.println("wrong guess, you have " + this.guesses + " guesses left");
+
+                if (this.guesses == 0) {
+                    System.out.println("you lose");
+                    x = false;
                 }
             }
             
