b7c1025df070246df3d81ce1a9f910e85868896b
xi0ni <george.robokos@gmail.com>
2025-07-23 11:16:26 -0400

    obj files by hand

--- Python/test.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-
-data = np.random.randint(0, 256, (100, 100, 3), dtype=np.uint8)
-img = Image.fromarray(data, "RGB")
-
-img.save("image.png")
-img.show()
