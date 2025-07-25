a397a8d9267dec1fc60507b96c9e900d361e7d83
xi0ni <george.robokos@gmail.com>
2025-07-25 11:07:13 -0400

    bowling ball in right place and day 5 classwork

--- Python/Tufts practice/Bowling/bowling_ball.py
+++ Python/Tufts practice/Bowling/bowling_ball.py
@@ -1,11 +1,18 @@
 import pygame
-from main import *
+from constants import RED, BLACK,WHITE
 
-
-class bowling_ball():
+class Bowling_ball(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         width = 40
         height = 60
-        self.image = pygame.Surface([width, height])
-        self.image.fill(RED)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, WHITE, (width // 2, height // 2), 20)
+        self.rect = self.image.get_rect()
 
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def move(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
--- Python/Tufts practice/Bowling/main.py
+++ Python/Tufts practice/Bowling/main.py
@@ -1,19 +1,29 @@
 import pygame
+from bowling_ball import Bowling_ball
+from constants import BLUE
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 800
 
+pygame.init()
 size = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size)
 done = False
+clock = pygame.time.Clock()
 
-while done == False:
+ball = Bowling_ball()
+
+
+while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    screen.fill(BLUE)
+    ball.draw(screen)
+    pygame.display.flip()
+    ball.move(400, 500)
+
+
+pygame.quit()
